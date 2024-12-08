e6afb2f85343a549f5c969cf09a459a773f3a753
최미래 <miraechoi@dgsw.hs.kr>
2024-12-08 23:16:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# elasticache-asynchronous-messaging
---
+++ src/main/java/com/elasticache/messaging/ElasticacheAsynchronousMessageApplication.java
@@ -0,0 +1,13 @@
+package com.elasticache.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticacheAsynchronousMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticacheAsynchronousMessageApplication.class, args);
+    }
+
+}
