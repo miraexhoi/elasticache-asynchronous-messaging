f856e97eeda5b99b7098d049f8ba483aeb7a4e30
최미래 <miraechoi@dgsw.hs.kr>
2024-12-09 20:38:54 +0900

    feature: add redis properties

---
+++ src/main/java/com/elasticache/messaging/global/RedisProperties.java
@@ -0,0 +1,14 @@
+package com.elasticache.messaging.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("spring.data.redis")
+public class RedisProperties {
+    private String host;
+    private int port;
+
+}
